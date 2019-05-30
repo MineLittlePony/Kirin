1ab76bd3634386c14fc165f2ddc1c2d5b5e55751
Sollace <sollacea@gmail.com>
2019-05-30 15:25:44 +0200

    Fixed sliders

--- src/main/java/com/minelittlepony/common/client/gui/element/Slider.java
+++ src/main/java/com/minelittlepony/common/client/gui/element/Slider.java
@@ -3,6 +3,8 @@ package com.minelittlepony.common.client.gui.element;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import net.minecraft.client.MinecraftClient;
+
 import com.minelittlepony.common.client.gui.IField;
 
 import java.util.function.Function;
@@ -30,7 +32,7 @@ public class Slider extends Button implements IField<Float, Slider> {
 
         this.min = min;
         this.max = max;
-        this.value = value;
+        this.value = convertFromRange(value, min, max);
     }
 
     @Override
@@ -41,51 +43,65 @@ public class Slider extends Button implements IField<Float, Slider> {
 
     public Slider setFormatter(@Nonnull Function<Float, String> formatter) {
         this.formatter = formatter;
-        setMessage(formatter.apply(getValue()));
+        getStyle().setText(formatter.apply(getValue()));
 
         return this;
     }
 
     @Override
     public Slider setValue(Float value) {
-        value = clamp(value, min, max);
-        value = (value - min) / (max - min);
-
-        if (value != this.value) {
-            this.value = action.perform(value);
-        }
-
-        if (formatter != null) {
-        	setMessage(formatter.apply(getValue()));
-        }
+        setClampedValue(convertFromRange(value, min, max));
 
         return this;
     }
 
+    protected void setClampedValue(float value) {
+        value = clamp(value, 0, 1);
+
+        if (value != this.value) {
+            this.value = value;
+
+            value = convertFromRange(action.perform(getValue()), min, max);
+            if (value != this.value) {
+                this.value = value;
+
+                if (formatter != null) {
+                    getStyle().setText(formatter.apply(getValue()));
+                }
+            }
+        }
+    }
+
+    private void onChange(double mouseX) {
+        // convert pixel coordinate to range (0 - 1)
+        setClampedValue((float)(mouseX - (x + 4)) / (float)(width - 8));
+    }
+
     @Override
     public Float getValue() {
-        return value;
+        return convertToRange(value, min, max);
     }
 
     @Override
     public void onClick(double mouseX, double mouseY) {
         super.onClick(mouseX, mouseY);
-        setValue((float)mouseX - (x + 4) / (float)(width - 8));
+        onChange(mouseX);
     }
 
     @Override
     protected void onDrag(double mouseX, double mouseY, double mouseDX, double mouseDY) {
-        setValue((float)mouseX - (x + 4) / (float)(width - 8));
+        onChange(mouseX);
     }
 
     @Override
-    public void render(int mouseX, int mouseY, float partialTicks) {
-        super.render(mouseX, mouseY, partialTicks);
+    protected void renderBg(MinecraftClient mc, int mouseX, int mouseY) {
+        mc.getTextureManager().bindTexture(WIDGETS_LOCATION);
 
-        int i = active ? 2 : 1;
-        // drawTexture
-        blit(x + (int)(value * (width - 8)), y, 0, 46 + i * 20, 4, 20);
-        blit(x + (int)(value * (width - 8)) + 4, y, 196, 46 + i * 20, 4, 20);
+        int i = 46 + (isHovered() ? 2 : 1) * 20;
+        int sliderX = x + (int)(value * (width - 8));
+
+        blit(sliderX,     y, 0,   i, 4, 20);
+        blit(sliderX + 4, y, 196, i, 4, 20);
     }
 
     @Override
@@ -93,7 +109,15 @@ public class Slider extends Button implements IField<Float, Slider> {
         return 0;
     }
 
-    protected float clamp(float value, float min, float max) {
+    static float convertFromRange(float value, float min, float max) {
+        return (clamp(value, min, max) - min) / (max - min);
+    }
+
+    static float convertToRange(float value, float min, float max) {
+        return clamp(min + (value * (max - min)), min, max);
+    }
+
+    static float clamp(float value, float min, float max) {
         return value < min ? min : value > max ? max : value;
     }
 }
