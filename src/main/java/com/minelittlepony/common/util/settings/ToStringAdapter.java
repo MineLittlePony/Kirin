38041435ce17efa1ca8ec59e7b7548a64bd2dee8
Sollace <sollacea@gmail.com>
2019-07-17 09:42:08 +0200

    Allow specifying a toString method for the ToStringAdapter

--- src/main/java/com/minelittlepony/common/util/settings/ToStringAdapter.java
+++ src/main/java/com/minelittlepony/common/util/settings/ToStringAdapter.java
@@ -3,27 +3,32 @@ package com.minelittlepony.common.util.settings;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.function.Function;
-
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 public final class ToStringAdapter<T> extends TypeAdapter<T> {
 
-    private final Function<String, T> converter;
+    private final Function<T, String> toString;
+    private final Function<String, T> fromString;
+
+    public ToStringAdapter(Function<T, String> toString, Function<String, T> fromString) {
+        this.toString = toString;
+        this.fromString = fromString;
+    }
 
     public ToStringAdapter(Function<String, T> converter) {
-        this.converter = converter;
+        this(Objects::toString, converter);
     }
 
     @Override
     public void write(JsonWriter out, T value) throws IOException {
-        out.value(value == null ? null : Objects.toString(value));
+        out.value(value == null ? null : toString.apply(value));
     }
 
     @Override
     public T read(JsonReader in) throws IOException {
         String value = in.nextString();
-        return value == null ? null : converter.apply(value);
+        return value == null ? null : fromString.apply(value);
     }
 }
