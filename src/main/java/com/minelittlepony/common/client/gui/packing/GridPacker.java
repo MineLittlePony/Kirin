5cc1936b661beebe28ad181dfb20c74c02d0928e
Sollace <sollacea@gmail.com>
2019-08-28 16:28:21 +0200

    Align contents horizontally when using the grid packer

--- src/main/java/com/minelittlepony/common/client/gui/packing/GridPacker.java
+++ src/main/java/com/minelittlepony/common/client/gui/packing/GridPacker.java
@@ -9,6 +9,8 @@ public class GridPacker implements IPacker {
 
     private int itemSpacing;
 
+    private int alignmentOffset;
+
     public GridPacker setListWidth(int width) {
         screen.width = width;
 
@@ -35,20 +37,31 @@ public class GridPacker implements IPacker {
 
     @Override
     public void start() {
+        alignmentOffset = computeAlignmentOffset();
         bounds.top = 0;
-        bounds.left = -(bounds.width + itemSpacing);
+        bounds.left = -(bounds.width + itemSpacing) + alignmentOffset;
     }
 
     @Override
     public Bounds next() {
         bounds.left += bounds.width + itemSpacing;
 
-        if (bounds.left + bounds.width > screen.width) {
-            bounds.left = 0;
+        if (bounds.left + bounds.width - alignmentOffset >= screen.width - 30) {
+            bounds.left = alignmentOffset;
             bounds.top += bounds.height + itemSpacing;
         }
 
         return bounds;
+    }
+
+    protected int computeAlignmentOffset() {
+        float maxWidth = screen.width - 30;
+        float colWidth = bounds.width + itemSpacing;
+        float maxCols = (float) Math.floor(maxWidth / colWidth);
+
+        float computedWidth = (bounds.width * maxCols) + (itemSpacing * (maxCols - 1));
+
+        return (int)Math.floor(maxWidth/2 - computedWidth/2);
     }
 
 }
