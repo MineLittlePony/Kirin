ebed4b37525bb202dd766febc5f4620a50e90617
Sollace <sollacea@gmail.com>
2019-10-04 22:17:37 +0200

    Let's try not to strip opacity from texture regions. In fact, let's not do it for the entire file. Fixes #1

--- src/main/java/com/minelittlepony/common/mixin/MixinSkinRemappingImageFilter.java
+++ src/main/java/com/minelittlepony/common/mixin/MixinSkinRemappingImageFilter.java
@@ -7,12 +7,16 @@ import net.minecraft.client.texture.SkinRemappingImageFilter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SkinRemappingImageFilter.class)
 public abstract class MixinSkinRemappingImageFilter implements ImageFilter {
 
     private static final String FILTER_IMAGE = "filterImage(Lnet/minecraft/client/texture/NativeImage;)Lnet/minecraft/client/texture/NativeImage;";
+
+    private static final String STRIP_COLOR = "method_3311(Lnet/minecraft/client/texture/NativeImage;IIII)V";
+    private static final String STRIP_ALPHA = "method_3312(Lnet/minecraft/client/texture/NativeImage;IIII)V";
 
     private boolean isLegacy;
 
@@ -26,4 +30,16 @@ public abstract class MixinSkinRemappingImageFilter implements ImageFilter {
         // convert skins from mojang server
         SkinFilterCallback.EVENT.invoker().processImage(ci.getReturnValue(), isLegacy);
     }
+
+    // Sorry, Mahjon. Input validation is good 'n all, but this interferes with out other mods.
+    @Inject(method = STRIP_ALPHA, at = @At("HEAD"), cancellable = true)
+    private static void cancelAlphaStrip(NativeImage image, int beginX, int beginY, int endX, int endY, CallbackInfo info) {
+        info.cancel();
+    }
+
+    @Inject(method = STRIP_COLOR, at = @At("HEAD"), cancellable = true)
+    private static void cancelColorStrip(NativeImage image, int beginX, int beginY, int endX, int endY, CallbackInfo info) {
+        info.cancel();
+    }
+    // -
 }
