cfe0c18f0c56ff77f8ed67688387b4d0e63ab205
Sollace <sollacea@gmail.com>
2019-11-24 18:40:54 +0200

    Added an accessor for the entity renderers

--- src/main/java/com/minelittlepony/common/mixin/MixinEntityRenderDispatcher.java
+++ src/main/java/com/minelittlepony/common/mixin/MixinEntityRenderDispatcher.java
@@ -6,11 +6,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.entity.EntityType;
 
 @Mixin(EntityRenderDispatcher.class)
 public interface MixinEntityRenderDispatcher {
 
     @Accessor("modelRenderers")
     Map<String, PlayerEntityRenderer> getPlayerRenderers();
+
+    @Accessor("renderers")
+    Map<EntityType<?>, EntityRenderer<?>> getEntityRenderers();
 }
