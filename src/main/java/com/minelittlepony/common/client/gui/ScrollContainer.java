c3e2d897db7268c9e1568c0a96bd8f55d82a3684
Sollace <sollacea@gmail.com>
2023-05-29 17:20:42 +0100

    Fix some scroll positioning issues

--- src/main/java/com/minelittlepony/common/client/gui/ScrollContainer.java
+++ src/main/java/com/minelittlepony/common/client/gui/ScrollContainer.java
@@ -91,14 +91,25 @@ public class ScrollContainer extends GameGui {
 
             drawBackground(context, mouseX, mouseY, partialTicks);
 
+            matrices.push();
+
             Padding padding = getContentPadding();
             matrices.push();
             matrices.translate(padding.left, 0, 0);
-
             verticalScrollbar.render(context,
                     mouseX - margin.left,
                     mouseY - margin.top,
-                    partialTicks);
+                    partialTicks
+            );
+            matrices.pop();
+            matrices.push();
+            matrices.translate(0, padding.top, 0);
+            horizontalScrollbar.render(context,
+                    mouseX - margin.left,
+                    mouseY - margin.top,
+                    partialTicks
+            );
+            matrices.pop();
 
             matrices.translate(
                     getScrollX(),
