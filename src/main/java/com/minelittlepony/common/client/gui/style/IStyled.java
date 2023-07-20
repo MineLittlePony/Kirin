69d22a9dc8eb30031bb4889e2845b7c6a1253d12
Sollace <sollacea@gmail.com>
2023-07-20 20:56:14 +0100

    Added IStyled.styled(...) helper method

--- src/main/java/com/minelittlepony/common/client/gui/style/IStyled.java
+++ src/main/java/com/minelittlepony/common/client/gui/style/IStyled.java
@@ -1,6 +1,12 @@
 package com.minelittlepony.common.client.gui.style;
 
+import java.util.function.Function;
+
 public interface IStyled<T extends IStyled<T>> extends IStyleFactory {
+
+    default T styled(Function<Style, Style> styleChange) {
+        return setStyle(styleChange.apply(getStyle()));
+    }
 
     default T setStyle(IStyleFactory factory) {
         return setStyle(factory.getStyle());
