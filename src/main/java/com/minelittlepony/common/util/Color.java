a244cf199bea227ae737df81d9e68f5b84d16e19
Sollace <sollacea@gmail.com>
2023-09-26 20:13:29 +0100

    Use mojang's colour utilities

--- src/main/java/com/minelittlepony/common/util/Color.java
+++ src/main/java/com/minelittlepony/common/util/Color.java
@@ -1,50 +1,51 @@
 package com.minelittlepony.common.util;
 
+import net.minecraft.util.math.ColorHelper;
+
 /**
  * Colouration Utilities
  */
 public interface Color {
-
     /**
      * Returns the ALPHA channel for the given colour hex code.
      */
     static float a(int hex) {
-        return (hex >> 24 & 255) / 255F;
+        return ColorHelper.Argb.getAlpha(hex);
     }
 
     /**
      * Returns the RED channel for the given colour hex code.
      */
     static float r(int hex) {
-        return (hex >> 16 & 255) / 255F;
+        return ColorHelper.Argb.getRed(hex);
     }
 
     /**
      * Returns the GREEN channel for the given colour hex code.
      */
     static float g(int hex) {
-        return (hex >> 8 & 255) / 255F;
+        return ColorHelper.Argb.getGreen(hex);
     }
 
     /**
      * Returns the BLUE channel for the given colour hex code.
      */
     static float b(int hex) {
-        return (hex & 255) / 255F;
+        return ColorHelper.Argb.getBlue(hex);
     }
 
     /**
      * Converts the given rgb floats on a range of 0-1 into a colour hex code.
      */
     static int argbToHex(float a, float r, float g, float b) {
-        return argbToHex((int)(a * 255), (int) (r * 255), (int) (g * 255), (int) (b * 255));
+        return ColorHelper.Argb.getArgb((int)(a * 255), (int) (r * 255), (int) (g * 255), (int) (b * 255));
     }
 
     /**
      * Converts the given rbg int on a range of 0-255 into a colour hex code.
      */
     static int argbToHex(int a, int r, int g, int b) {
-        return (a << 24) | (r << 16) | (g << 8) | (b);
+        return ColorHelper.Argb.getArgb(a, r, g, b);
     }
 
     /**
